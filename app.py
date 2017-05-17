8cb21993d0b972ea8eda544949df0b2d1afc0fef
Winston Venderbush <winstonvenderbush@gmail.com>
2017-05-17 14:21:53 -0400

    updated app

--- app.py
+++ app.py
@@ -12,7 +12,19 @@ def root():
 
 @app.route("/settings")
 def settings():
-    return render_tempalte("settings.html")
+    return render_template("settings.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
