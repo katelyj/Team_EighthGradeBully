4e2845cf5b26c77b64733daa3ecd02af08ed5be8
Patrick Chan <patrickchan1999@gmail.com>
2017-05-26 22:17:49 -0400

    Rewrote interface, removed circular import, and wrote ajax function

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template, request, session, url_for
 
-from utils.security import security
-from utils.views import public_views
+from lib.security import security
+from lib.views import public_views
+from lib.views import ajax_views
 
 app = Flask(__name__)
 app.secret_key = "patrickchan"
 app.register_blueprint(security.security)
 app.register_blueprint(public_views.public_views)
+app.register_blueprint(ajax_views.ajax_views)
 
 
 @app.route("/settings")
 def settings():
-	if ("username" in session):
-		username = session["username"]
+    if ("username" in session):
+        username = session["username"]
         return render_template('settings.html', user=username, login="True")
-	return redirect(url_for('/'))
+    else:
+        return redirect(url_for('/'))
 
 if __name__ == '__main__':
     app.debug = True
