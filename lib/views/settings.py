700f72962df6b139cf481fdd1909c4f4a22786d5
katelyj <kjohnston@stuy.edu>
2017-05-30 14:44:20 -0400

    settings routing

--- lib/views/settings.py
+++ lib/views/settings.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, session, url_for
 
 settings_views = Blueprint('settings_views', __name__)
 
-@public_views.route('/')
+@settings_views.route('/settings')
 def settings():
     if ("username" in session):
         username = session["username"]
