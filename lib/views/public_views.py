7b9b0aa85165bec4865cd4947b7bfd5ead16ce4b
Patrick Chan <patrickchan1999@gmail.com>
2017-05-30 20:40:42 -0400

    Removed the hardcoded table and utilize the iterator of the Schedule class to generate the table on the fly

--- lib/views/public_views.py
+++ lib/views/public_views.py
@@ -1,11 +1,13 @@
 import datetime
 from flask import Blueprint, render_template, session, url_for
 
+from lib.Schedule import Schedule
+
 public_views = Blueprint('public_views', __name__)
 
 @public_views.route('/')
 def home():
-    schedule = "Regular Schedule"
+    schedule = Schedule.Schedule('fall-14-regular')
     now = datetime.datetime.now()
     midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
     seconds = (now - midnight).seconds
@@ -18,7 +20,7 @@ def home():
     if ("username" in session):
         username = session["username"]
         return render_template('schedule.html', starttime=str(seconds), user=username, schedulename=username+"'s " + schedule, login="True")
-    return render_template('schedule.html', starttime=str(seconds), schedulename=schedule)
+    return render_template('schedule.html', starttime=str(seconds), schedule=schedule)
 
 @public_views.route('/about')
 def about():
