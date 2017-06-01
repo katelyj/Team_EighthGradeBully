25db82cf3d3a00b0b5abe10cfc93906c5a2c6400
katelyj <kjohnston@stuy.edu>
2017-06-01 14:45:11 -0400

    minor settings things

--- lib/views/settings_views.py
+++ lib/views/settings_views.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, render_template, session, url_for
+from flask import Blueprint, render_template, session, url_for, redirect
 
 settings_views = Blueprint('settings_views', __name__)
 
-@settings_views.route('/settings')
+@settings_views.route('/settings', methods = ['GET', 'POST'])
 def settings():
     if ("username" in session):
         username = session["username"]
         return render_template('settings.html', user=username, login="True")
     else:
         return render_template('settings.html')
+
+@settings_views.route('/changesettings/', methods = ['GET', 'POST'])
+def change_settings():
+    return redirect(url_for('settings_views.settings'))
