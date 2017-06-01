abd13f44833c6010a9c53b0fabb2c8a851d6c334
katelyj <kjohnston@stuy.edu>
2017-06-01 14:51:57 -0400

    devlog

--- lib/views/settings_views.py
+++ lib/views/settings_views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, session, url_for, redirect
 
+
 settings_views = Blueprint('settings_views', __name__)
+
 
 @settings_views.route('/settings', methods = ['GET', 'POST'])
 def settings():
@@ -16,4 +18,6 @@ def change_schedule():
 
 @settings_views.route('/changepassword/', methods = ['GET', 'POST'])
 def change_password():
+    old_pass = session.get('old_pass')
+    new_pass = session.get('new_pass')
     return redirect(url_for('settings_views.settings'))
