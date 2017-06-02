b112a39605e62aef39e6b16abaa478d8c50e2003
Maddie Ostergaard <Mostergaard@stuy.edu>
2017-06-02 14:25:35 -0400

    connecting frontend forms to backend

--- lib/views/settings_views.py
+++ lib/views/settings_views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, session, url_for, redirect
 settings_views = Blueprint('settings_views', __name__)
 
 
-@settings_views.route('/settings', methods = ['GET', 'POST'])
+@settings_views.route('/settings/', methods = ['GET', 'POST'])
 def settings():
     if ("username" in session):
         username = session["username"]
@@ -14,10 +14,15 @@ def settings():
 
 @settings_views.route('/changeschedule/', methods = ['GET', 'POST'])
 def change_schedule():
+	d = request.form()
+	with open('changed_schedule.txt', 'a') as file_:
+		file_.write(str(d))
+
     return redirect(url_for('settings_views.settings'))
 
 @settings_views.route('/changepassword/', methods = ['GET', 'POST'])
 def change_password():
-    old_pass = session.get('old_pass')
-    new_pass = session.get('new_pass')
+    d = request.form()
+    old_pass = d['old_pass']
+    new_pass = d['new_pass']
     return redirect(url_for('settings_views.settings'))
