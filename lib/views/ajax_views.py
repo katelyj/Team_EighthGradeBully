1af51cd8034d3111226022db3671f40a16df5d70
Patrick Chan <patrickchan1999@gmail.com>
2017-06-06 01:39:48 -0400

    Added ajax call for retrieving schedule and wrote function for saving schedule

--- lib/views/ajax_views.py
+++ lib/views/ajax_views.py
@@ -1,12 +1,16 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../'))
-from lib.Schedule import Schedule
+from lib.Schedule import Schedule, UserScheduleDBManager
 
 ajax_views = Blueprint('ajax_views', __name__)
 
 @ajax_views.route('/schedule_jsonify/<schedule_name>')
 def schedule_jsonify(schedule_name):
     return jsonify(Schedule.Schedule(schedule_name).get_schedule_jsonify())
+
+@ajax_views.route('/user_schedule/')
+def user_schedule_jsonify():
+    return jsonify(UserScheduleDBManager.UserScheduleDBManager().retrieve_user_schedule(session.get('username')))
--- lib/views/settings_views.py
+++ lib/views/settings_views.py
@@ -2,22 +2,30 @@ from flask import Blueprint, render_template, session, url_for, redirect, reques
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../")) 
+sys.path.insert(0, os.path.abspath("../"))
 from lib.security import AuthManager, security
-from lib.Schedule import Schedule
+from lib.Schedule import Schedule, UserScheduleDBManager
 
 settings_views = Blueprint('settings_views', __name__)
 
-
-@settings_views.route('/settings/', methods = ['GET', 'POST'])
+@settings_views.route('/settings/', methods=['GET'])
 def settings():
     return render_template('settings.html', is_logged_in=security.is_logged_in, is_admin=security.is_admin)
 
-@settings_views.route('/changeschedule/', methods = ['GET', 'POST'])
+@settings_views.route('/changeschedule/', methods=['POST'])
 def change_schedule():
-    d = request.form()
-    with open('changed_schedule.txt', 'a') as file_:
-	file_.write(str(d))
+    user_schedule_db_manager = UserScheduleDBManager.UserScheduleDBManager()
+
+    data = request.form
+    schedule_data = ''
+
+    for key, value in data.items():
+        schedule_data += '{key}:{value}'.format(key=key, value=value)
+        schedule_data += '~'
+    schedule_data.strip('~')
+
+    user_schedule_db_manager.insert_user_schedule(session.get('username'), schedule_data)
+    print user_schedule_db_manager.retrieve_user_schedule(session.get('username'))
     return redirect(url_for('settings_views.settings'))
 
 @settings_views.route('/changepassword/', methods = ['GET', 'POST'])
@@ -32,5 +40,5 @@ def change_password():
         return redirect(url_for('settings_views.settings'))
     else:
         AuthManager.AuthManager().change_pass(user, old_pass, new_pass, conf_new_pass)
-        
+
     return redirect(url_for('settings_views.settings'))
