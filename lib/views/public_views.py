9069256893254ab58238175b664041ff73904d22
Winston Venderbush <winstonvenderbush@gmail.com>
2017-06-06 01:20:35 -0400

    error handling

--- lib/views/public_views.py
+++ lib/views/public_views.py
@@ -9,6 +9,11 @@ public_views = Blueprint('public_views', __name__)
 
 @public_views.route('/')
 def home():
+    now = datetime.now()
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    seconds = (now - midnight).seconds
+
+
     weekday_to_string = {1: 'mon',
                          2: 'tues',
                          3: 'wed',
@@ -20,14 +25,17 @@ def home():
     weekly_schedule_db_manager = WeeklyScheduleDBManager.WeeklyScheduleDBManager()
     weekly_schedule = weekly_schedule_db_manager.retrieve_weekly_schedule(date)
     weekday_name = weekday_to_string[today_date.weekday() + 1]
-    today_data = weekly_schedule[weekday_name]
-    schedule = Schedule.Schedule(today_data[0])
-    a_b_day = today_data[1]
 
-    now = datetime.now()
-    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
-    seconds = (now - midnight).seconds
-    return render_template('schedule.html', starttime=str(seconds), schedule=schedule, a_b_day=a_b_day, is_logged_in=security.is_logged_in, is_admin=security.is_admin)
+    if (weekly_schedule == None):
+        return render_template('schedule.html', starttime=str(seconds), schedule=None, a_b_day="C", is_logged_in=security.is_logged_in, is_admin=security.is_admin)
+
+    else:
+        today_data = weekly_schedule[weekday_name]
+        schedule = Schedule.Schedule(today_data[0])
+        a_b_day = today_data[1]
+        return render_template('schedule.html', starttime=str(seconds), schedule=schedule, a_b_day=a_b_day, is_logged_in=security.is_logged_in, is_admin=security.is_admin)
+
+    
 
 @public_views.route('/about')
 def about():
